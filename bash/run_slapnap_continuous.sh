9c5c2a20cd6f53993b8cd84b1618e964b4330749
bdwilliamson <brianw26@uw.edu>
2021-10-26 09:15:49 -0700

    Update control scripts

--- bash/run_slapnap_continuous.sh
+++ bash/run_slapnap_continuous.sh
@@ -17,22 +17,10 @@ for NAB in ${ALL_NABS[@]}
 do
   SLAPNAP_NAB=$(echo $NAB | sed 's/+/;/g' | sed 's/ //g')
   DIR_NAB=$(echo $NAB | tr '[:upper:]' '[:lower:]' | sed 's/+/_/g' | sed 's/ //g')
-  mkdir -p ./docker_output/$DIR_NAB
-  if grep -q ";" <<< "$SLAPNAP_NAB"; then
-      outcomes="estsens;multsens"
-  else
-      outcomes="sens"
-  fi
-  # run variable importance for a subset of mAbs
-  if grep -q "VRC07-523-LS;PGT121" <<< "$SLAPNAP_NAB"; then
-      importance_grp="cond;marg"
-      importance_ind="marg;pred"
-  else
-      importance_grp=""
-      importance_ind=""
-  fi
+  mkdir -p ./docker_output/continuous/$DIR_NAB
+  outcomes="ic80"
   sudo docker run \
-    -v "$(pwd)"/docker_output/$DIR_NAB/:/home/output \
+    -v "$(pwd)"/docker_output/continuous/$DIR_NAB/:/home/output \
     -e learners="rf;lasso;xgboost" \
     -e cvperf="TRUE" \
     -e cvtune="TRUE" \
@@ -43,7 +31,7 @@ do
     -e importance_grp=$importance_grp \
     -e importance_ind=$importance_ind \
     -e var_thresh="0;4" \
-    -e return="report;data;learner;figures;vimp" \
+    -e return="report;data;learner;figures" \
     -e nfolds="5" \
     slapnap
 done
