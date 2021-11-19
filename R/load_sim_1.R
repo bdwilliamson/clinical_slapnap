de30ab290aa676bf06d2dfd054be9b9e336b145b
bdwilliamson <brianw26@uw.edu>
2021-11-19 15:39:58 -0800

    Update sim 1 plot with Peter's feedback

--- R/load_sim_1.R
+++ R/load_sim_1.R
@@ -5,27 +5,49 @@ library("data.table")
 library("cowplot")
 theme_set(theme_cowplot())
 
+summary_statistics <- readRDS(here::here("R_output", "summary_statistics_simulation_1.rds"))
+
 # read in results of the simulation, create a table
 all_output_files <- list.files(here::here("R_output", "simulation_1"))
 all_output <- tibble::tibble(data.table::rbindlist(
   lapply(as.list(here::here("R_output", "simulation_1", all_output_files)), readRDS)
-))
+)) %>% 
+  left_join(summary_statistics %>% 
+              mutate(datatype = ifelse(outcome == "ic80", "continuous", "binary"),
+                     bnab = toupper(bnab),
+                     bnab = gsub("10E8V4", "10E8v4", bnab),
+                     prediction_performance = est) %>% 
+              select(bnab, datatype, prediction_performance), 
+            by = c("bnab", "datatype")) %>% 
+  mutate(bnab = gsub("_", " + ", bnab),
+         bnab = ifelse(bnab == "VRC01-PGDM1400-10E8v4", "VRC01/PGDM1400/10E8v4", bnab))
 mc_vars <- all_output %>% 
   group_by(bnab, epsilon, datatype, augmented) %>% 
-  summarize(mc_var = var(est), .groups = "drop") %>% 
+  summarize(mc_var = var(est), 
+            pred_perf = mean(prediction_performance), .groups = "drop") %>% 
   pivot_wider(names_from = augmented, values_from = mc_var,
               names_prefix = "aug_") %>% 
   mutate(relative_efficiency = aug_FALSE / aug_TRUE,
-         outcome = ifelse(datatype == "binary", "IC80 < 1", "IC80"))
+         outcome = ifelse(datatype == "binary", "IC80 < 1", "IC80"),
+         percentage = factor(epsilon, levels = c(0.5, 1, 2), labels = c("50%", "100%", "200%")),
+         bnab = factor(bnab, levels = c(
+           "VRC01", "PGT121", "VRC07-523-LS", "VRC07-523-LS + 10-1074",
+           "VRC07-523-LS + PGT121", "VRC07-523-LS + PGDM1400",
+           "VRC07-523-LS + PGT121 + PGDM1400", "VRC01/PGDM1400/10E8v4"
+         )))
 
 
 rel_eff_plot <- mc_vars %>% 
-  ggplot(aes(x = factor(epsilon), y = relative_efficiency, color = bnab)) +
-  geom_point(position = position_dodge(width = 0.5)) +
+  ggplot(aes(x = pred_perf, y = relative_efficiency, color = bnab)) +
+  geom_point() +
   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
-  ylim(c(0.75, 2.5)) +
-  labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)", x = "Fraction of auxiliary sequences", color = "bnAb") +
-  facet_grid(rows = vars(outcome), labeller = label_both)
+  # ylim(c(0.75, 2.5)) +
+  labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)", 
+       x = "Prediction Performance", color = "bnAb") +
+  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Percentage Increase in Available Viruses",
+                     breaks = NULL, labels = NULL)) +
+  facet_grid(rows = vars(outcome), cols = vars(percentage), labeller = label_both,
+             scales = "free_y")
 
 ggsave(filename = here::here("R_output", "sim_1_rel_eff.png"),
        plot = rel_eff_plot, 
