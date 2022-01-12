a5a3b7f1fceb92a615b709966698bc75ccd5126b
bdwilliamson <brianw26@uw.edu>
2022-01-12 14:11:44 -0800

    Handle xgboost and library errors
    Previously, didn't have all library algos
    loaded into the environment. Now we do. Also,
    handled xgboost 'invalid handle' error by: (1)
    harmonizing xgboost version (1.4.1.1) from SLAPNAP
    call with current version; and (2) calling xgb.Booster.complete
    on all SL.xgboost* functions prior to requesting predictions.

--- R/simulation_1b.R
+++ R/simulation_1b.R
@@ -1,4 +1,4 @@
-# Simulation 1a:
+# Simulation 1b:
 # Data analysis comparing
 # (a) using sequences with data only in CATNAP to
 # (b) using sequences with data in LANL, with SLAPNAP predictions
@@ -15,18 +15,19 @@ library("xgboost")
 
 source(here("R", "00_sim_utils.R"))
 source(here("R", "00_utils.R"))
+source(here("R", "03_super_learner_libraries.R"))
 
 # get command-line args --------------------------------------------------------
 # these specify the bnAb of interest, min number of readouts to specify the country(ies) of interest,
 # and the outcome type (binary or continuous)
 parser <- ArgumentParser()
-parser$add_argument("--bnab", default = "PGT121", help = "the bnAb of interest")
-parser$add_argument("--country_threshold", type = "double", default = "20", help = "min number of neutralization readouts for consideration")
-parser$add_argument("--outcome", default = "ic80", help = "the outcome of interest")
+parser$add_argument("--bnab", default = "VRC07-523-LS_PGT121", help = "the bnAb of interest")
+parser$add_argument("--country-threshold", type = "double", default = "20", help = "min number of neutralization readouts for consideration")
+parser$add_argument("--outcome", default = "sens", help = "the outcome of interest")
 parser$add_argument("--output-dir", default = here::here("R_output", "simulation_1b"), help = "the output directory")
 args <- parser$parse_args()
 
-print(paste0("Running bnAb ", args$bnab))
+print(paste0("Running bnAb ", args$bnab, "; outcome ", args$outcome))
 
 outcome_type <- ifelse(args$outcome == "ic80", "continuous", "binary")
 
@@ -35,16 +36,25 @@ outcome_type <- ifelse(args$outcome == "ic80", "continuous", "binary")
 dat <- read_csv(here::here("dat", paste0("slapnap_lanl_", args$bnab, "_wcountry.csv")))
 
 # redefine the outcome of interest, remove unneccessary variables
-refined_dat <- dat %>%
-  select(-seq.id.lanl, -seq.id.catnap, -num.seqs.in.country.catnap, -num.seqs.in.country.all,
-         -contains("ic50"), -contains("censored")) %>%
-  rename(ic80 = !!(paste0(args$bnab, ".ic80.imputed")))
-
-if (args$outcome == "sens") {
-  refined_dat <- refined_dat %>%
-    mutate(sens = ic80 < 1) %>%
-    select(-ic80)
+if (!any(grepl("_", args$bnab))) {
+  # handle single bnAb
+  refined_dat <- dat %>%
+    select(-seq.id.lanl, -seq.id.catnap, -num.seqs.in.country.catnap, -num.seqs.in.country.all,
+           -seq.in.catnap, -seq.in.lanl.2019, -contains("ic50"), -contains("censored")) %>%
+    rename(ic80 = contains("ic80")) %>%
+    mutate(sens = as.numeric(ic80 < 1), .after = "ic80")
+  n_ab <- 1
+} else {
+  # handle bnAb regimen
+  refined_dat <- dat %>%
+    select(-seq.id.lanl, -seq.id.catnap, -num.seqs.in.country.catnap, -num.seqs.in.country.all,
+           -seq.in.catnap, -seq.in.lanl.2019, -contains("ic50"), -contains("ic80"),
+           -contains("iip"), -contains("dichotomous")) %>%
+    mutate(ic80 = dat %>% pull(pc.ic80),
+           sens = as.numeric(ic80 < 1), .after = "country.iso")
+  n_ab <- 2
 }
+
 # define the countries of interest
 all_countries <- as.list(unique(dat$country.iso))
 countries_above_threshold <- unlist(lapply(all_countries, function(country) {
@@ -53,22 +63,63 @@ countries_above_threshold <- unlist(lapply(all_countries, function(country) {
 }))
 countries_of_interest <- unlist(all_countries)[countries_above_threshold]
 
+# remove ic80 if outcome is sens; otherwise, remove sens
+if (args$outcome == "sens") {
+  refined_dat <- refined_dat %>%
+    select(-ic80)
+} else {
+  refined_dat <- refined_dat %>%
+    select(-sens)
+}
+
+if (n_ab == 1) {
+  outcome_txt <- args$outcome
+} else {
+  if (args$outcome == "sens") {
+    outcome_txt <- "estsens"
+  } else {
+    outcome_txt <- "ic80"
+  }
+}
+
 # get predictions from SLAPNAP--------------------------------------------------
 # read in the correct model
 slapnap_folder <- here::here("docker_output",
-                             paste0(switch(as.numeric(outcome_type == "continuous") + 1, NULL, "continuous"), "/", tolower(args$bnab)))
+                             paste0(switch(as.numeric(outcome_type == "continuous") + 1,
+                                           NULL, "continuous/"),
+                                    tolower(args$bnab)))
 all_learner_files <- list.files(slapnap_folder,
-                           pattern = paste0("learner_", args$outcome, "_", args$bnab))
+                           pattern = paste0("learner_", outcome_txt, "_", args$bnab))
 learner_files <- all_learner_files[!grepl("cv", all_learner_files)]
-dates <- as.Date(gsub(paste0("learner_", args$outcome, "_", args$bnab, "_"), "", gsub(".rds", "", learner_files)),
+dates <- as.Date(gsub(paste0("learner_", outcome_txt, "_", args$bnab, "_"), "",
+                      gsub(".rds", "", learner_files)),
                     format = "%d%b%Y")
 current_date <- Sys.Date()
 closest_date <- which.min(current_date - dates)
 slapnap_mod <- readRDS(paste0(slapnap_folder, "/", learner_files[closest_date]))
 
+# create the correct learner/screen combinations
+if (any(grepl("var_thresh", slapnap_mod$libraryNames))) {
+    thresh_init <- gsub(".*?var_thresh_(.*?)", "", slapnap_mod$libraryNames)
+    unique_thresh <- unique(as.numeric(thresh_init[!grepl("SL", thresh_init)]))
+    for (i in seq_len(length(unique_thresh))) {
+        make_screen_wrapper(var_thresh = unique_thresh[i])
+    }
+}
+# if there were any xgboosts fit, need to "complete" them
+is_xgboost <- grepl("xgboost", slapnap_mod$libraryNames)
+if (any(is_xgboost)) {
+    all_xgboosts <- slapnap_mod$libraryNames[is_xgboost]
+    for (i in seq_len(length(all_xgboosts))) {
+        slapnap_mod$fitLibrary[is_xgboost][[i]]$object <- xgb.Booster.complete(slapnap_mod$fitLibrary[is_xgboost][[i]]$object)
+    }
+}
+
 # run the analyses -------------------------------------------------------------
+set.seed(123125)
 output <- NULL
-for (country in countries_of_interest) {
+for (i in seq_len(length(countries_of_interest))) {
+  country <- countries_of_interest[i]
   country_data <- refined_dat %>%
     filter(country.iso == country) %>%
     select(-country.iso)
@@ -106,3 +157,5 @@ for (country in countries_of_interest) {
 }
 
 saveRDS(output, paste0(args$output_dir, "/", args$bnab, "_", args$outcome, ".rds"))
+
+print("Analysis complete.")
