672889f32b70598c2fcb6f6353a238f8c71f6032
Brian Williamson <bwillia2@fredhutch.org>
2022-07-19 09:48:56 -0700

    Update plot from sim 1b

--- R/load_sim_1b.R
+++ R/load_sim_1b.R
@@ -21,40 +21,66 @@ all_output <- tibble::tibble(data.table::rbindlist(
             by = c("bnab", "outcome")) %>% 
   mutate(bnab = gsub("_", " + ", bnab),
          bnab = ifelse(bnab == "VRC01-PGDM1400-10E8v4", "VRC01/PGDM1400/10E8v4", bnab))
-mc_vars <- all_output %>% 
-  group_by(bnab, epsilon, datatype, augmented) %>% 
-  summarize(mc_var = var(est), 
-            pred_perf = mean(prediction_performance), .groups = "drop") %>% 
-  pivot_wider(names_from = augmented, values_from = mc_var,
+ci_widths <- all_output %>% 
+  select(-est, -starts_with("ci")) %>% 
+  # group_by(bnab, country, n_catnap, n_overall, datatype, augmented) %>% 
+  pivot_wider(names_from = augmented, values_from = width,
               names_prefix = "aug_") %>% 
   mutate(relative_efficiency = aug_FALSE / aug_TRUE,
          outcome = ifelse(datatype == "binary", "IC80 < 1", "IC80"),
-         percentage = factor(epsilon, levels = c(0.5, 1, 2), labels = c("50%", "100%", "200%")),
+         # percentage = factor(epsilon, levels = c(0.5, 1, 2), labels = c("50%", "100%", "200%")),
          bnab = factor(bnab, levels = c(
            "VRC01", "VRC07-523-LS", "PGT121", "VRC26.25", "PGDM1400",
            "VRC07-523-LS + PGT121", "VRC07-523-LS + VRC26.25", "VRC07-523-LS + PGDM1400", "VRC07-523-LS + 10-1074",
            "VRC07-523-LS + PGT121 + PGDM1400", "VRC01/PGDM1400/10E8v4"
-         )))
+         )),
+         country = factor(country, levels = c(
+           "CN", "DE", "KE", "MW", "TZ", "UG", "US", "ZA"
+         ), labels = c(
+           "China", "Germany", "Kenya", "Malawi", "Tanzania", "Uganda", "United States", "South Africa"
+         )),
+         `AUC` = factor(case_when(
+           outcome == "IC80 < 1" & prediction_performance <= 0.7 ~ "1",
+           outcome == "IC80 < 1" & prediction_performance > 0.7 & prediction_performance <= 0.8 ~ "2",
+           outcome == "IC80 < 1" & prediction_performance > 0.8 ~ "3",
+         ), levels = c("1", "2", "3"), labels = c("[0.5, 0.7]", "(0.7, 0.8]", "(0.8, 1]")),
+         `R-squared` = factor(case_when(
+           outcome == "IC80" & prediction_performance <= 0.2 ~ "1",
+           outcome == "IC80" & prediction_performance > 0.2 & prediction_performance <= 0.4 ~ "2",
+           outcome == "IC80" & prediction_performance > 0.4 ~ "3",
+         ), levels = c("1", "2", "3"), labels = c("[0, 0.2]", "(0.2, 0.4]", "[0.4, 1]")),
+         excess_prop = (n_overall - n_catnap) / n_catnap * 100)
 
-continuous_rel_eff_plot <- mc_vars %>% 
+continuous_rel_eff_plot <- ci_widths %>% 
   filter(outcome == "IC80") %>% 
-  ggplot(aes(x = pred_perf, y = relative_efficiency, color = bnab)) +
-  geom_point() +
+  ggplot(aes(x = excess_prop, y = relative_efficiency, color = bnab, shape = country)) +
+  geom_point(size = 2.5) +
+  scale_shape_manual(values = c(49:56)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
   labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)", 
-       x = "Prediction Performance", color = "bnAb") +
-  scale_x_continuous(sec.axis = sec_axis(~ ., name = "Percentage Increase in Available Viruses",
-                                         breaks = NULL, labels = NULL)) +
-  facet_grid(rows = vars(outcome), cols = vars(percentage), labeller = label_both)
+       x = "Proportion of Additional Information in LANL", color = "bnAb",
+       shape = "Country") +
+  facet_grid(rows = vars(outcome), cols = vars(`R-squared`), labeller = label_both)
 
-binary_rel_eff_plot <- mc_vars %>% 
+binary_rel_eff_plot <- ci_widths %>% 
   filter(outcome == "IC80 < 1") %>% 
-  ggplot(aes(x = pred_perf, y = relative_efficiency, color = bnab)) +
-  geom_point() +
+  ggplot(aes(x = excess_prop, y = relative_efficiency, color = bnab, shape = country)) +
+  geom_point(size = 2.5) +
+  scale_shape_manual(values = c(49:56)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
   labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)", 
-       x = "Prediction Performance", color = "bnAb") +
-  facet_grid(rows = vars(outcome), cols = vars(percentage), labeller = label_both)
+       x = "Proportion of Additional Information in LANL", color = "bnAb",
+       shape = "Country") +
+  facet_grid(rows = vars(outcome), cols = vars(`AUC`), labeller = label_both)
+# binary_rel_eff_plot <- ci_widths %>% 
+#   filter(outcome == "IC80 < 1") %>% 
+#   ggplot(aes(x = excess_prop, y = relative_efficiency, color = bnab, shape = country)) +
+#   geom_point() +
+#   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
+#   labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)", 
+#        x = "Proportion of Additional Information in LANL", color = "bnAb", shape = "Country") +
+#   scale_shape_manual(values = shapes) +
+#   facet_grid(rows = vars(outcome), labeller = label_both)
 
 lgnd <- get_legend(continuous_rel_eff_plot)
 ylab <- get_y_axis(continuous_rel_eff_plot)
@@ -64,24 +90,14 @@ full_plot <- plot_grid(
     theme(axis.line.x = element_blank(), axis.line.y = element_blank()),
   plot_grid(
     continuous_rel_eff_plot + labs(x = NULL, y = NULL) + theme(legend.position = "none"),
-    binary_rel_eff_plot + labs(x = NULL, y = NULL) + theme(legend.position = "none",
-                                                           strip.text.x = element_blank()),
-    nrow = 2, ncol = 1
+    binary_rel_eff_plot + labs( y = NULL) + theme(legend.position = "none"),
+    # binary_rel_eff_plot + labs( y = NULL) + theme(legend.position = "none",
+    #                                                        strip.text.x = element_blank()),
+    nrow = 2, ncol = 1, rel_heights = c(0.9, 1)
   ),
   lgnd, nrow = 1, ncol = 3, rel_widths = c(.05, 1, .6)
 )
 
-# rel_eff_plot <- mc_vars %>%
-#   ggplot(aes(x = pred_perf, y = relative_efficiency, color = bnab)) +
-#   geom_point() +
-#   geom_hline(yintercept = 1, linetype = "dashed", color = "red") +
-#   labs(y = "Relative Efficiency (ignoring vs using auxiliary sequences)",
-#        x = "Prediction Performance", color = "bnAb") +
-#   scale_x_continuous(sec.axis = sec_axis(~ ., name = "Percentage Increase in Available Viruses",
-#                      breaks = NULL, labels = NULL)) +
-#   facet_grid(rows = vars(outcome), cols = vars(percentage), labeller = label_both,
-#              scales = "free")
-
-ggsave(filename = here::here("R_output", "sim_1_rel_eff.png"),
+ggsave(filename = here::here("R_output", "sim_1b_rel_eff.png"),
        plot = full_plot, 
-       width = 9, height = 5, units = "in")
+       width =  10, height = 5, units = "in")
