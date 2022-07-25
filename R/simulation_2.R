86f17ef301859390089775e9a206a911eb1eb6f2
Brian Williamson <bwillia2@fredhutch.org>
2022-07-25 11:49:28 -0700

    Differentiate site-scanning and priority

--- R/simulation_2.R
+++ R/simulation_2.R
@@ -32,20 +32,21 @@ if (!is.na(Sys.getenv("RSTUDIO", unset = NA))) {
 nreps_per_combo <- args$nreps_total / args$nreps_per_job
 
 # read in data from AMP
-sens_data_from_amp <- readr::read_csv(here::here("dat", "resis_resid_full_v2.csv"))
+sens_data_from_amp <- readr::read_csv(here::here("dat", "resis_resid_full_v3.csv"))
 
 # define "important" AA positions in gp120 (from AMP sieve analysis plan)
 positions <- sens_data_from_amp %>% filter(preselected.site == 1) %>% pull(pos)
-# from Craig: the positions passing minimum variability filter
-minvar_positions <- sens_data_from_amp$pos
-all_positions <- sens_data_from_amp %>% filter(pos %in% minvar_positions) %>% pull(pos)
-J <- nrow(sens_data_from_amp)
+# the positions passing minimum variability filter (>= 4 sequences that differ from most common
+# at a given site)
+sens_data_from_amp_minvar <- sens_data_from_amp %>%
+  filter(minvar.pooled >= 4)
+all_positions <- sens_data_from_amp_minvar %>% pull(pos)
+J <- nrow(sens_data_from_amp_minvar)
 
 # proportions of infected subjects in amp with putative sensitive genotype at each position, each tx group
-gammas <- sens_data_from_amp %>%
+gammas <- sens_data_from_amp_minvar %>%
   select(pos, prop.pooled.placebo.w.sens) %>%
-  rename(prop_placebo = prop.pooled.placebo.w.sens) %>%
-  filter(pos %in% minvar_positions)
+  rename(prop_placebo = prop.pooled.placebo.w.sens)
 
 # note that at site 230, residue is D, which was actually resistant
 gammas[gammas$pos == 230, ]$prop_placebo <- 1 - gammas[gammas$pos == 230, ]$prop_placebo
@@ -60,7 +61,6 @@ q <- 0.1
 ns <- c(2071, 4141, 12422)
 # incidence among placebo-arm subjects; rate per person-year
 lambda_0s <- c(.03, .018, .006) # from Gilbert (2019), but we need smaller for higher PE
-# lambda_0s <- c(0.018, 0.009, 0.003)
 
 # set up the other simulation parameters
 param_grid <- expand.grid(mc_id = seq_len(nreps_per_combo), n = ns,
@@ -80,8 +80,6 @@ print(paste0("Running n = ", current_dynamic_args$n, " for the ", args$analysis,
 current_seed <- job_id + current_dynamic_args$n + as.numeric(args$analysis == "site-scanning") * 1e4
 print(current_seed)
 set.seed(current_seed)
-# approx. 38 seconds per replication for site-scanning (n = 4000).
-# approx. 1 second per replication for priority (n = 4000).
 if (.Platform$OS.type == "windows") {
   library("parallel")
   num_cores <- parallel::detectCores()
@@ -97,7 +95,7 @@ if (.Platform$OS.type == "windows") {
       run_sim2_once(mc_id = i + args$nreps_per_job * (current_dynamic_args$mc_id - 1),
                     n = current_dynamic_args$n, all_positions = all_positions, gamma = gammas,
                     lambda_0 = current_dynamic_args$lambda_0, beta = log(1 - pe_overall),
-                    pe_1 = current_dynamic_args$pe_1, pe_0 = current_dynamic_args$pe_0, 
+                    pe_1 = current_dynamic_args$pe_1, pe_0 = current_dynamic_args$pe_0,
                     eos = 365 * 2, q = q,
                     site_scanning = (args$analysis == "site-scanning"),
                     positions = positions, position = which(gammas$pos == 230),
@@ -120,7 +118,7 @@ if (.Platform$OS.type == "windows") {
     run_sim2_once(mc_id = i + args$nreps_per_job * (current_dynamic_args$mc_id - 1),
                   n = current_dynamic_args$n, all_positions = all_positions, gamma = gammas,
                   lambda_0 = current_dynamic_args$lambda_0, beta = log(1 - pe_overall),
-                  pe_1 = current_dynamic_args$pe_1, pe_0 = current_dynamic_args$pe_0, 
+                  pe_1 = current_dynamic_args$pe_1, pe_0 = current_dynamic_args$pe_0,
                   eos = 365 * 2, q = q,
                   site_scanning = (args$analysis == "site-scanning"),
                   positions = positions, position = which(gammas$pos == 230),
@@ -134,9 +132,9 @@ output <- tibble::as_tibble(data.table::rbindlist(output_lst))
 # round(colMeans(output))
 output %>%
   group_by(position) %>%
-  summarize(sieve_power = mean(reject, na.rm = TRUE), 
+  summarize(sieve_power = mean(reject, na.rm = TRUE),
             unadj_sieve_power = mean(unadjusted_p_val < 0.05, na.rm = TRUE),
             overall_power = mean(hr_p < 0.05, na.rm = TRUE),
             n_events = mean(n_events, na.rm = TRUE),
             n00 = mean(n00), n01 = mean(n01), n10 = mean(n10), n11 = mean(n11))
-saveRDS(output, file = paste0(args$output_dir, "/output_", job_id, ".rds"))
+saveRDS(output, file = paste0(args$output_dir, "/output_", args$analysis, "_", job_id, ".rds"))
